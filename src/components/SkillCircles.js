b512da2119505758ac5fe84441599a8d8ed510be
Rin Raeuber <mail@rin-raeuber.com>
2021-01-13 17:55:05 +0100

    Fix: Use autosizer for width of bubble canvas.

--- src/components/SkillCircles.js
+++ src/components/SkillCircles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import { useQuery, gql } from '@apollo/client';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 const GET_SKILLS = gql`
   query GetSkills {
@@ -25,7 +26,7 @@ const CIRCLE_COLORS = [
 const SkillCircles = () => {
   // Amelia Wattenberger has some good thoughts on how to combine React and D3
   // -> https://wattenberger.com/blog/react-and-d3
-  // but for now I use https://github.com/weknowinc/react-bubble-chart-d3/
+  // but for now we use https://github.com/weknowinc/react-bubble-chart-d3/
 
   const { loading, error, data } = useQuery(GET_SKILLS);
 
@@ -34,28 +35,31 @@ const SkillCircles = () => {
 
   return (
     <div className="pb-6 w-full">
-      <BubbleChart
-        graph={{ zoom: 0.5, offsetX: 0, offsetY: 0 }}
-        showLegend={false}
-        valueFont={{ size: 0 }}
-        // TODO: this having a fixed size is not great –
-        // should use something like https://github.com/bvaughn/react-virtualized-auto-sizer
-        width={1000}
-        height={400}
-        legendPercentage={0}
-        labelFont={{
-          family: 'Helvetica',
-          size: 18,
-          color: '#fff',
-          weight: 'normal',
-        }}
-        // bubbleClickFunc={this.bubbleClick}
-        data={data.skills.map(({ title, strength }, index) => ({
-          label: title,
-          value: strength,
-          color: CIRCLE_COLORS[index],
-        }))}
-      />
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <BubbleChart
+            graph={{ zoom: 0.5, offsetX: 0, offsetY: 0 }}
+            showLegend={false}
+            valueFont={{ size: 0 }}
+            width={width}
+            height={400}
+            legendPercentage={0}
+            labelFont={{
+              family: 'Helvetica',
+              size: 18,
+              color: '#fff',
+              weight: 'normal',
+            }}
+            // TODO: Edit skill
+            // bubbleClickFunc={this.bubbleClick}
+            data={data.skills.map(({ title, strength }, index) => ({
+              label: title,
+              value: strength,
+              color: CIRCLE_COLORS[index],
+            }))}
+          />
+        )}
+      </AutoSizer>
     </div>
   );
 };
