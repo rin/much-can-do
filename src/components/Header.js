c01d7928309756acbb49788ec0d7ff22a446aef2
Rin Raeuber <mail@rin-raeuber.com>
2021-01-13 18:56:28 +0100

    Fix: Simpler layout for header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,9 @@
 const Header = () => (
-  <div className="max-w-7xl mx-auto">
-    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-      <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"></div>
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Much Can Do</span>
-          </h1>
-        </div>
-      </main>
+  <div className="w-full">
+    <div className="text-center pt-8">
+      <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <span className="block">Much Can Do</span>
+      </h1>
     </div>
   </div>
 );
